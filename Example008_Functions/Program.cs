e8d47caddd54093d9c6424283f6dd2130036ea81
Ruslan Fakhrutdinov <fakhrutdinov.consulting@gmail.com>
2022-09-13 18:33:53 +0300

    Example008_Functions created

---
+++ Example008_Functions/Program.cs
@@ -0,0 +1,18 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int a1 = 15;
+int b1 = 21;
+int c1 = 896578;
+int a2 = 12;
+int b2 = 345;
+int c2 = 1234;
+int a3 = 54;
+int b3 = 98;
+int c3 = 100;
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+Console.WriteLine(max);
